dd0ea1721b27f40274b8883f121a5e5348dd616f
imsed <imsed@users.noreply.github.com>
2023-06-06 12:26:29 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0" # This is an example Amazon Linux 2 AMI ID; replace it with the appropriate AMI ID for your region
+  ami           = "ami-0c55b159cbfafe1f0" # This is an example Amazon Linux  AMI ID; replace it with the appropriate AMI ID for your region
   instance_type = "t2.micro"
 
   tags = {
